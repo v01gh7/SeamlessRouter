feeadd62dd8428bf3e18cdcb87ed9f9d535059a7
VOIGHT <moriorpandemic@gmail.com>
2025-10-30 13:43:20 +0500

    refactor(router): modularize router logic into dedicated files

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,5 +1,12 @@
+import { attachRoutesListeners } from "./Router/attachRouteListeners"
+import { gatherRoutes } from "./Router/gatherRoutes"
+import { navigate } from "./Router/navigation"
+
 export class Router {
   init() {
+    const routes = gatherRoutes()
+    attachRoutesListeners(routes, navigate)
     console.log('Router initialized')
   }
 }
+
