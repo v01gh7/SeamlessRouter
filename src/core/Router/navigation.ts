213115a203f17aa8b4cac8d61a0d8a6eb81cefd1
VOIGHT <moriorpandemic@gmail.com>
2025-10-30 16:05:18 +0500

    refactor(router): modularize navigate logic and add safe fallback

--- src/core/Router/navigation.ts
+++ src/core/Router/navigation.ts
@@ -1,6 +1,15 @@
-export const navigate = (url: string) => {
+import { safeNavigateFallback } from "@core/utils";
+import { loadPage } from "./loadPage";
+
+export const navigate = async (url: string) => {
     console.log('Навигация к:', url);
     history.pushState({}, '', url);    
+    const res = await loadPage(url);
+    if (!res) {
+    safeNavigateFallback(url);
+    return;
+    }
+
 }
 
 window.addEventListener('popstate', () => {
