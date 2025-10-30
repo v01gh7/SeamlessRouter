876404ac6f3ae3dbba880ddfdb46ea8874e32860
VOIGHT <moriorpandemic@gmail.com>
2025-10-30 14:59:57 +0500

    feat(router): support history navigation with popstate listener

--- src/core/Router/navigation.ts
+++ src/core/Router/navigation.ts
@@ -2,3 +2,7 @@ export const navigate = (url: string) => {
     console.log('Навигация к:', url);
     history.pushState({}, '', url);    
 }
+
+window.addEventListener('popstate', () => {
+    navigate(window.location.pathname);
+});
