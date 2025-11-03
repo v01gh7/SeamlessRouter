10517f121769430c446ae99a611f0bacb411e7c9
VOIGHT <moriorpandemic@gmail.com>
2025-11-03 14:04:32 +0500

    feat(router/runScripts): add full cleanup of scripts from DOM and global sandbox before execution

--- src/core/Router/runScripts.ts
+++ src/core/Router/runScripts.ts
@@ -1,6 +1,8 @@
+import { purgeAllScripts } from "@core/Sandbox/sandbox";
 import { appendFreshScript, shouldSkip, shouldPreserve, extractScripts } from "@core/utils/dom";
 
 export const runScripts = (newBody: HTMLElement) => {
+    purgeAllScripts();
     const scripts: HTMLScriptElement[] = extractScripts(newBody as HTMLBodyElement);
 
     for (const script of scripts) {
