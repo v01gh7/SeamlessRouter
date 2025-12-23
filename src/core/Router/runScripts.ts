132ceedfb92388024aeacfbb4cc4a6eb98ed658b
VOIGHT <moriorpandemic@gmail.com>
2025-12-23 17:37:42 +0500

    fix: clear script cache after purging scripts

--- src/core/Router/runScripts.ts
+++ src/core/Router/runScripts.ts
@@ -1,8 +1,9 @@
 import { purgeAllScripts } from "@core/Sandbox/sandbox";
-import { appendFreshScript, shouldSkip, shouldPreserve, extractScripts } from "@core/utils/dom";
+import { appendFreshScript, shouldSkip, shouldPreserve, extractScripts, clearScriptCache } from "@core/utils/dom";
 
 export const runScripts = (newBody: HTMLElement) => {
     purgeAllScripts();
+    clearScriptCache(); // Очищаем кэш скриптов после удаления
     const scripts: HTMLScriptElement[] = extractScripts(newBody as HTMLBodyElement);
 
     for (const script of scripts) {
